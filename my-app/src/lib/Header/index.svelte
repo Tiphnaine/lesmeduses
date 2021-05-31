ae227155b15a3b642b4fb0ec390d3afd884dcb52
Tiphaine <b00775519@essec.edu>
2021-05-31 19:24:40 +0200

    Fix : Change Header

--- my-app/src/lib/Header/index.svelte
+++ my-app/src/lib/Header/index.svelte
@@ -6,18 +6,19 @@
 <header class="text-gray-600 body-font">
 	<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 	  <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-		<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
+		<!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
 		  <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-		</svg>
-		<span class="ml-3 text-xl">Tailblocks</span>
+		</svg> -->
+		<img src="https://i.imgur.com/ugjoMVJ.png" alt="Les Méduses" class="max-h-9 max-w-9" />
+		<span class="ml-3 text-xl">Les Méduses</span>
 	  </a>
 	  <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-		<a class="mr-5 hover:text-gray-900">First Link</a>
-		<a class="mr-5 hover:text-gray-900">Second Link</a>
-		<a class="mr-5 hover:text-gray-900">Third Link</a>
-		<a class="mr-5 hover:text-gray-900">Fourth Link</a>
+		<a sveltekit:prefetch href="/" class="mr-5 hover:text-gray-900">Accueil</a>
+		<a sveltekit:prefetch href="/about" class="mr-5 hover:text-gray-900">About</a>
+		<a sveltekit:prefetch href="/todos" class="mr-5 hover:text-gray-900">Todos</a>
+		<a class="mr-5 hover:text-gray-900">Marche pas encore lol</a>
 	  </nav>
-	  <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+	  <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Contact
 		<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
 		  <path d="M5 12h14M12 5l7 7-7 7"></path>
 		</svg>
@@ -25,7 +26,7 @@
 </div>
 </header>
 
-<header>
+<!-- <header>
 	<div class="corner">
 		<a href="https://kit.svelte.dev">
 			<img src={logo} alt="SvelteKit" />
@@ -47,9 +48,8 @@
 	</nav>
 
 	<div class="corner">
-		<!-- TODO put something else here? github link? -->
 	</div>
-</header>
+</header> -->
 
 <style>
 	header {
