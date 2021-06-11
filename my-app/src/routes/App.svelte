9c2827dbc0c779e77911040dbfdf4e10bb572027
LouiseOger <b00755673@essec.edu>
2021-06-11 16:54:27 +0200

    pasmieuxquavant

--- my-app/src/routes/App.svelte
+++ my-app/src/routes/App.svelte
@@ -1,111 +1,46 @@
-<script>
-	import { scaleLinear } from 'd3-scale';
-	import points from './data.js';
 
-	const yTicks = [0, 2, 4, 6, 8];
-	const xTicks = [1980, 1990, 2000, 2010];
-	const padding = { top: 20, right: 15, bottom: 20, left: 25 };
+        <div class="flex items-center justify-center py-8 px-4">
+            <div class="w-11/12 lg:w-2/3">
+                <div class="flex flex-col justify-between h-full">
+                    <div>
+                        <div class="lg:flex w-full justify-between">
+                            <h3 class="text-gray-600 dark:text-gray-400 leading-5 text-base md:text-xl font-bold">Selling Overview</h3>
+                            <div class="flex items-center justify-between lg:justify-start mt-2 md:mt-4 lg:mt-0">
+                                <div class="flex items-center">
+                                    <button class="py-2 px-4 bg-gray-100 dark:bg-gray-700 rounded ease-in duration-150 text-xs text-gray-600 dark:text-gray-400 hover:bg-gray-200">Dollars</button>
+                                    <button class="py-2 px-4 bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded text-white ease-in duration-150 text-xs hover:bg-indigo-600">Tickets</button>
+                                </div>
+                                <div class="lg:ml-14">
+                                    <div class="bg-gray-100 dark:bg-gray-700 ease-in duration-150 hover:bg-gray-200 pb-2 pt-1 px-3 rounded-sm">
+                                        <select aria-label="select year"  class="text-xs text-gray-600 dark:text-gray-400 bg-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 rounded">
+                                            <option class="leading-1">Year</option>
+                                            <option class="leading-1">2020</option>
+                                            <option class="leading-1">2019</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex items-end mt-6">
+                            <h3 class="text-indigo-500 leading-5 text-lg md:text-2xl">$65,875</h3>
+                            <div class="flex items-center md:ml-4 ml-1 text-green-700">
+                                <p class="text-green-700 text-xs md:text-base">17%</p>
+                                <svg role="img" class="text-green-700" aria-label="increase. upward arrow icon" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                                    <path d="M6 2.5V9.5" stroke="currentColor" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M8 4.5L6 2.5" stroke="currentColor" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round"></path>
+                                    <path d="M4 4.5L6 2.5" stroke="currentColor" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-6">
+                        <canvas id="myChart" width="1025" height="400" role="img" aria-label="line graph to show selling overview in terms of months and numbers" ></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
 
-	let width = 500;
-	let height = 200;
-
-	$: xScale = scaleLinear()
-		.domain([minX, maxX])
-		.range([padding.left, width - padding.right]);
-
-	$: yScale = scaleLinear()
-		.domain([Math.min.apply(null, yTicks), Math.max.apply(null, yTicks)])
-		.range([height - padding.bottom, padding.top]);
-
-	$: minX = points[0].x;
-	$: maxX = points[points.length - 1].x;
-	$: path = `M${points.map(p => `${xScale(p.x)},${yScale(p.y)}`).join('L')}`;
-	$: area = `${path}L${xScale(maxX)},${yScale(0)}L${xScale(minX)},${yScale(0)}Z`;
-
-	function formatMobile (tick) {
-		return "'" + tick.toString().slice(-2);
-	}
-</script>
-
-<h2>Arctic sea ice minimum</h2>
-
-<div class="chart" bind:clientWidth={width} bind:clientHeight={height}>
-	<svg>
-		<!-- y axis -->
-		<g class="axis y-axis" transform="translate(0, {padding.top})">
-			{#each yTicks as tick}
-				<g class="tick tick-{tick}" transform="translate(0, {yScale(tick) - padding.bottom})">
-					<line x2="100%"></line>
-					<text y="-4">{tick} {tick === 8 ? ' million sq km' : ''}</text>
-				</g>
-			{/each}
-		</g>
-
-		<!-- x axis -->
-		<g class="axis x-axis">
-			{#each xTicks as tick}
-				<g class="tick tick-{ tick }" transform="translate({xScale(tick)},{height})">
-					<line y1="-{height}" y2="-{padding.bottom}" x1="0" x2="0"></line>
-					<text y="-2">{width > 380 ? tick : formatMobile(tick)}</text>
-				</g>
-			{/each}
-		</g>
-
-		<!-- data -->
-		<path class="path-area" d={area}></path>
-		<path class="path-line" d={path}></path>
-	</svg>
-</div>
-
-<p>Average September extent. Source: <a href='https://climate.nasa.gov/vital-signs/arctic-sea-ice/'>NSIDC/NASA</a></p>
-
-<style>
-	.chart, h2, p {
-		width: 100%;
-		max-width: 500px;
-		margin-left: auto;
-		margin-right: auto;
-	}
-
-	svg {
-		position: relative;
-		width: 100%;
-		height: 200px;
-		overflow: visible;
-	}
-
-	.tick {
-		font-size: .725em;
-		font-weight: 200;
-	}
-
-	.tick line {
-		stroke: #aaa;
-		stroke-dasharray: 2;
-	}
-
-	.tick text {
-		fill: #666;
-		text-anchor: start;
-	}
-
-	.tick.tick-0 line {
-		stroke-dasharray: 0;
-	}
-
-	.x-axis .tick text {
-		text-anchor: middle;
-	}
-
-	.path-line {
-		fill: none;
-		stroke: rgb(0,100,100);
-		stroke-linejoin: round;
-		stroke-linecap: round;
-		stroke-width: 2;
-	}
-
-	.path-area {
-		fill: rgba(0,100,100,0.2);
-	}
-</style>
+        
+        
+    
