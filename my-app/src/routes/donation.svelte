3b04ca9658bf84723e9b39f811ae04e170d866bf
Tiphaine <b00775519@essec.edu>
2021-06-14 18:13:18 +0200

    Clean donation page

--- my-app/src/routes/donation.svelte
+++ my-app/src/routes/donation.svelte
@@ -6,10 +6,10 @@
 
 	<section class="text-gray-600 body-font">
 		<div class="container px-5 py-24 mx-auto flex flex-wrap">
-		  <h2 class="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5">This portal doesn't work for now as we are not available to take any donations for the moment, but thank you for thinking of us !</h2>
+		  <h2 class="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5">This portal doesn't work for now as we are not available to take any donations for the moment, but thank you for thinking of us!</h2>
 		  <div class="md:w-3/5 md:pl-6">
 			<p class="leading-relaxed text-base">The association Les Méduses needs your donations
-				To allow us to create events and train the different members of ESSEC we need funds !</p>
+				To allow us to create events and train the different members of ESSEC we need funds!</p>
 			<div class="flex md:mt-4 mt-6">
 			  <a href="https://www.facebook.com/collectiflesmeduses" class="text-indigo-500 inline-flex items-center ml-4">Learn More
 				<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
@@ -20,3 +20,38 @@
 		  </div>
 		</div>
 	  </section>
+
+	  <style>
+
+	  p {
+		--tw-text-opacity: 1;
+		color: rgba(75, 85, 99, var(--tw-text-opacity));
+		/* text-align:center; */
+		}
+	
+		h1 {
+		color : #552B4D;
+		}
+	
+		h2 {
+		color : #B366A4;
+		}
+	
+	  a {
+			color: var(--heading-color);
+			text-decoration: none;
+			transition: color 0.2s linear;
+		}
+	
+		a:hover {
+			color: #B366A4;
+		}
+		/* .welcome {
+			position: absolute;
+			width: 200px;
+		height: 200px;
+		} */
+	
+
+	</style>
+	
