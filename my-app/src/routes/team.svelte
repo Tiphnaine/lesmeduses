3e42edf84a00ed84e0c56537f707bf401620db1a
Tiphaine <b00775519@essec.edu>
2021-06-14 18:32:00 +0200

    Clean Team

--- my-app/src/routes/team.svelte
+++ my-app/src/routes/team.svelte
@@ -1,29 +1,21 @@
 <section class="text-gray-600 body-font"> 
-     
-  <div class="py-12">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-  
-      <div class="py-5 ">
+
+    <div class="py-5 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="lg:text-center">
-            
-            <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Few words from our team members</h2>
-            Why it matters to us
-            
+            <h2 class="text-base font-semibold tracking-wide uppercase">Few words from our team members</h2>
+            <p>Why it matters to us</p>
           </div>
         </div>
-      </div>
     </div>
-  </div>
     </section>
 
 <section>
-  <div class="testimonial-1 py-4 md:py-12">
+  <div class="testimonial-1 py-4 md:py-4">
     <div class="container mx-auto px-4">
         <div class="md:flex md:flex-wrap md:-mx-4 mb-4">
-  
             <div class="md:w-1/2 md:px-4 mt-6 md:mt-0">
-                <div class="testimonial p-6 border-2 border-solid flex hover:border-indigo-400 hover:bg-indigo-100 transition-colors duration-300">
+                <div class="testimonial p-6 border-2 border-solid flex hover:border-gray-600 hover:bg-gray-200 transition-colors duration-300">
                     <div class="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden mr-4 md:mr-6 flex-shrink-0">
                         <img src="src\lib\images\Charlotte.jpg" alt="Charlotte Caillat" class="w-full h-full object-cover">
                     </div>
@@ -37,7 +29,7 @@
             </div>
   
             <div class="md:w-1/2 md:px-4 mt-6 md:mt-0">
-                <div class="testimonial p-6 border-2 border-solid flex hover:border-indigo-400 hover:bg-indigo-100 transition-colors duration-300">
+                <div class="testimonial p-6 border-2 border-solid flex hover:border-gray-600 hover:bg-gray-200 transition-colors duration-300">
                     <div class="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden mr-4 md:mr-6 flex-shrink-0">
                         <img src="src\lib\images\Louise.jpg" alt="" class="w-full h-full object-cover">
                     </div>
@@ -50,20 +42,20 @@
             </div>
   
             <div class="md:w-1/2 md:px-4 mt-6">
-                <div class="testimonial p-6 border-2 border-solid flex hover:border-indigo-400 hover:bg-indigo-100 transition-colors duration-300">
+                <div class="testimonial p-6 border-2 border-solid flex hover:border-gray-600 hover:bg-gray-200 transition-colors duration-300">
                     <div class="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden mr-4 md:mr-6 flex-shrink-0">
                         <img src="src\lib\images\Tiphaine.jfif" alt="Tiphaine H." class="w-full h-full object-cover">
                     </div>
                     <div>
                         <p class="text-gray-600">"Finally an activist collective against gender-based violence at ESSEC ! We are convinced that thanks to our actions, things can get better and better."</p>
-                        <div class="text-gray-900 font-bold uppercase mt-6">- Tiphaine E.</div>
+                        <div class="text-gray-900 font-bold uppercase mt-6">- Tiphaine H.</div>
                         <div class="text-gray-600">Team Member, IT manager</div>
                     </div>
                 </div>
             </div>
   
             <div class="md:w-1/2 md:px-4 mt-6">
-              <div class="testimonial p-6 border-2 border-solid flex hover:border-indigo-400 hover:bg-indigo-100 transition-colors duration-300">
+              <div class="testimonial p-6 border-2 border-solid flex hover:border-gray-600 hover:bg-gray-200 transition-colors duration-300">
                   <div class="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden mr-4 md:mr-6 flex-shrink-0">
                       <img src="src\lib\images\Alice.jfif" alt="" class="w-full h-full object-cover">
                   </div>
@@ -87,9 +79,9 @@
 
 
 <section class="text-gray-600 body-font">
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-col text-center w-full mb-20">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our team!</h1>
+    <div class="container px-5 py-5 mx-auto">
+      <div class="flex flex-col text-center w-full mb-10">
+        <h1 class="font-sans mt-2 text-3xl leading-8 font-semibold tracking-tight sm:text-4xl my-4">Our team</h1>
         <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Les Méduses are a collective of more than 60 people, and it's always growing! So even though we can't present everyone, we're going to introduce the main actors of change.</p>
       </div>
       <div class="flex flex-wrap -m-4">
@@ -97,8 +89,8 @@
           <div class="flex relative">
             <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://i.imgur.com/kLxa1ze.jpg">
             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Charlotte Caillat</h2>
-              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The President</h1>
+              <h2 class="tracking-widest text-sm title-font font-medium mb-1">Charlotte Caillat</h2>
+              <h1 class="title-font text-lg font-medium mb-3">The President</h1>
               <p class="leading-relaxed">Charlotte created the collective with Tara MacKeown, and she is now leading it! She is the main interlocutor of ESSEC</p>
             </div>
           </div>
@@ -107,7 +99,7 @@
           <div class="flex relative">
             <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://i.imgur.com/stz0OWU.jpg">
             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Tara MacKeown</h2>
+              <h2 class="tracking-widest text-sm title-font font-medium mb-1">Tara MacKeown</h2>
               <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The General Secretary</h1>
               <p class="leading-relaxed">Tara created the collective with Charlotte Caillat, and she is now ensuring the unity of the collective and intern communication</p>
             </div>
@@ -117,7 +109,7 @@
           <div class="flex relative">
             <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://i.imgur.com/F81m6PB.jpg">
             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Claire Contri</h2>
+              <h2 class="tracking-widest text-sm title-font font-medium mb-1">Claire Contri</h2>
               <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Treasurer</h1>
               <p class="leading-relaxed">If you have any money issues, Claire is the person to come to. She is handling all the funds of the collective, and maybe of your donations! </p>
             </div>
@@ -127,7 +119,7 @@
           <div class="flex relative">
             <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://i.imgur.com/GaOpq2N.jpg">
             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Eliza Binder</h2>
+              <h2 class="tracking-widest text-sm title-font font-medium mb-1">Eliza Binder</h2>
               <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Head of Communication</h1>
               <p class="leading-relaxed">Eliza is heading the team in charge of updating social medias, this website, and every other part of the communication.</p>
             </div>
@@ -137,7 +129,7 @@
           <div class="flex relative">
             <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://i.imgur.com/K0N31CV.jpg">
             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Lucy Frémont</h2>
+              <h2 class="tracking-widest text-sm title-font font-medium mb-1">Lucy Frémont</h2>
               <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Head of Events</h1>
               <p class="leading-relaxed">Lucy is in charge of keeping Les Méduses visible to the students of ESSEC through different events also called "action coup de poing".</p>
             </div>
@@ -147,7 +139,7 @@
           <div class="flex relative">
             <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://i.imgur.com/lJVmyko.jpg">
             <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Justine Taillefer</h2>
+              <h2 class="tracking-widest text-sm title-font font-medium mb-1">Justine Taillefer</h2>
               <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Head of Partnerships</h1>
               <p class="leading-relaxed">Justine works with our many partners that help us in our actions : #NousToutes, ESSEC, UN Women, and many other associations.</p>
             </div>
@@ -157,7 +149,7 @@
             <div class="flex relative">
               <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="https://i.imgur.com/pncbyzv.jpg">
               <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">Ségolène Rousset</h2>
+                <h2 class="tracking-widest text-sm title-font font-medium mb-1">Ségolène Rousset</h2>
                 <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Head of strategies and Development</h1>
                 <p class="leading-relaxed">Ségolène is the one who is thinking of the future of the collective and looking at new ways to act and to improve the ESSEC's environment.</p>
               </div>
@@ -166,3 +158,38 @@
       </div>
     </div>
   </section>
+<style>
+  section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+  p {
+		width: 100%;
+    --tw-text-opacity: 1;
+    color: rgba(75, 85, 99, var(--tw-text-opacity));
+    /* text-align:center; */
+	}
+
+	h1 {
+		width: 100%;
+    color : #552B4D;
+	}
+
+	h2 {
+		width: 100%;
+    color : #B366A4;
+	}
+
+  a {
+		color: var(--heading-color);
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: #B366A4;
+	}
+  </style>
