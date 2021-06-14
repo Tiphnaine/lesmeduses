40f4fa5055d2c869914735489d5481dec985e212
Tiphaine <b00775519@essec.edu>
2021-06-14 18:59:33 +0200

    Change header "Les Méduses"

--- my-app/src/lib/Header/index.svelte
+++ my-app/src/lib/Header/index.svelte
@@ -5,13 +5,13 @@
 
 <header class="text-gray-600  bg-gray-200 body-font shadow-lg">
 	<div class="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
-	  <a class="font-sans flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+	  <p class="font-sans flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
 		<!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
 		  <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
 		</svg> -->
 		<img src="https://zupimages.net/up/21/22/8o3k.png" alt="Les Méduses" class="max-h-9 max-w-9" />
 		<span class="ml-3 text-xl">Les Méduses</span>
-	  </a>
+	</p>
 	  <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
 		<a sveltekit:prefetch href="/" class="font-sans mr-5 ">Homepage</a>
 		<a sveltekit:prefetch href="/donation" class="font-sans mr-5 ">Donations</a>
